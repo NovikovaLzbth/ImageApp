c85bd92e0a076843da7b248e0770b0c61988e604
Elizaveta <liza@Mac-mini-Aleksandr.local>
2025-01-20 13:57:27 +0300

    add button and image

--- Image/Image/ContentView.swift
+++ Image/Image/ContentView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let url = URL(string: "https://cs8.pikabu.ru/post_img/big/2016/02/04/7/145458292112119207.jpg")
+    
     var body: some View {
-        VStack {
-            Text("привте")
+        ZStack {
+            Color(.white).ignoresSafeArea()
+            
+            ZStack{
+                AsyncImage(url: url)
+                    .frame(width: 70, height: 900, alignment: .top)
+
+                Button(action: {
+                    print("изменение")
+                }, label: {
+                    Text("Change image")
+                        .font(.title)
+                        .padding()
+                        .background(Color.pink)
+                        .foregroundStyle(.white)
+                        .cornerRadius(40)
+                        .padding()
+                        .padding(-2)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 45)
+                                .stroke(.pink, lineWidth: 5)
+                        }
+                })
+                .frame(width: 250, height: 600, alignment: .bottom)
+            }
         }
-        .padding()
     }
 }
 
